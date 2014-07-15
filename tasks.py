c0c430bee0285769ca8b3463a069bb5ca0743c45
Chen Lin <clin_@berkeley.edu>
2014-07-15 16:32:55 -0700

    Cleaned up error handling in queue_email.

--- tasks.py
+++ tasks.py
@@ -12,14 +12,11 @@ def queue_email(self, from_email, to_email, subject, content):
     errors = []
     for client in clients.get_clients():
         try:
-            resp = client.send_email(from_email, to_email, subject, content)
+            client.send_email(from_email, to_email, subject, content)
         except clients.EmailClientException as e:
-            errors.append(e.args)
+            errors.append(e)
         else:
-            if resp:
-                # TODO: Update record to indicate result of operation
-                return client.__class__.__name__, resp
-            else:
-                errors.append(resp)
+            # TODO: Update record to indicate result of operation
+            return client.__class__.__name__
 
     raise self.retry(exc=Exception(*errors))
